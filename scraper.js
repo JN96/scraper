f33bb9d44279be8796c689c86eabf5099d656e65
James <james@DESKTOP-VVTSASE.localdomain>
2018-08-14 20:53:36 +0100

    first commit

---
+++ scraper.js
@@ -0,0 +1,66 @@
+var casper = require('casper').create();
+console.log("Loading script...");
+var links = [];
+
+function getLinks() {
+// scrape the href links from each product
+var links = document.querySelectorAll('ul.results-list li.c div.summary span a');
+return Array.prototype.map.call(links, function (e) {
+	return e.getAttribute('href');
+});
+}
+
+// opens tuiholidays deals page homepage
+casper.start('https://www.tuiholidays.ie/f/deals/summer-holidays', function () {
+	casper.then(function () {
+		console.log("gathering links...");
+		links = this.evaluate(getLinks);
+		casper.wait(500, function() {
+			console.log("gathering scraped data...");
+		});
+		this.each(links,function(self,url){
+			self.thenOpen("https://www.tuiholidays.ie"+url,function(a){
+				casper.wait(500, function() {
+				// console.log(this.getCurrentUrl());
+				var details = this.evaluate(function(){
+					var package_name = document.querySelector('.heading-section .pg-heading .dis-inblock h1').textContent;
+					
+					var location = document.querySelector('.heading-section .product-info .location span.pad-left-5').textContent;
+					// var location_splice = location.replace(/^\&nbsp\;|<br?\>*/gi, " ").replace(/\&nbsp\;|<br?\>$/gi, " ").trim();
+					
+					// var country = document.querySelector('.heading-section .product-info .location span.pad-left-5').innerHTML;
+					// var country_splice = " " + location_splice.split(" ").pop(); 
+
+					var price1 = document.querySelector('.part1').textContent;
+					var price2 = document.querySelector('.part2').textContent;
+					var price_pp = price1 + "." + price2;
+
+					var description = document.querySelector('.copy').textContent;
+					var source_name = document.title;
+					var source_link = window.location.href;
+
+					return "package_name: " + package_name +
+					"\nlocation: " + location +
+					"\nprice_pp: " + price_pp +
+					"\ndescription: " + description +
+					"\nsource_name: " + source_name +
+					"\nsource_link: " + source_link + "\n";
+
+					// return [package_name, location, price_pp, description, source_name, source_link];
+				});
+				console.log(details);
+			});
+			});
+		});
+	});
+});
+
+casper.run(function () {
+	// for(var i in links) {
+	// 	console.log(links[i]);
+	// }
+	// for(var i in data){
+	// 	console.log(data[i]);
+	// }
+	casper.exit();
+});
